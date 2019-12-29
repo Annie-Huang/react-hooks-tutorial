8f020d46cc5f4aa91eb68095627ffd1c0c2237da
Annie Huang <annie.hn@gmail.com>
2019-12-29 21:07:46 +1100

    Create the IntervalHookCounter.js. <IntervalClassCounter> in the incremental but not <IntervalHookCounter>

--- effect-hook/src/App.js
+++ effect-hook/src/App.js
@@ -6,6 +6,7 @@ import ClassMouse from "./components/ClassMouse";
 import HookMouse from "./components/HookMouse";
 import MouseContainer from "./components/MouseContainer";
 import IntervalClassCounter from "./components/IntervalClassCounter";
+import IntervalHookCounter from "./components/IntervalHookCounter";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         {/*<HookMouse />*/}
         {/*<MouseContainer />*/}
         <IntervalClassCounter />
+        <IntervalHookCounter/>
     </div>
   );
 }
