1ade961c9be0cf63dad88913db1f9aa57ec21bc1
Annie Huang <annie.hn@gmail.com>
2019-12-29 21:19:06 +1100

    Reticfy the problem by adding count as the watch list for useEffect.

--- effect-hook/src/components/IntervalHookCounter.js
+++ effect-hook/src/components/IntervalHookCounter.js
@@ -7,13 +7,17 @@ const IntervalHookCounter = () => {
         setCount(count + 1);
     };
 
+    // If you think dependent array as a way to specify when you want to run the useEffect, you will run into problems.
+    // Instead, dependency array should be thought as a way to let React know about everything that useEffect must watch
+    // for changes.
+    // So but enter [], we tell React don't watch any variable change, just do it for the initial rendering.
     useEffect(() => {
         const  interval = setInterval(tick, 1000);
 
         return () => {
             clearInterval(interval);
         }
-    }, []);
+    }, [count]);
 
     return (
         <div>
