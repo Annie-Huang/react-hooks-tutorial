6e9377e84764b6ba1c425e29ab38bde5fd4128a2
Annie Huang <annie.hn@gmail.com>
2019-12-29 21:25:31 +1100

    Another way to do this without putting count into the watch array. use the useState instead

--- effect-hook/src/components/IntervalHookCounter.js
+++ effect-hook/src/components/IntervalHookCounter.js
@@ -3,9 +3,14 @@ import React, {useState, useEffect} from 'react';
 const IntervalHookCounter = () => {
     const [count, setCount] = useState(0);
 
+    // const tick = () => {
+    //     setCount(count + 1);
+    // };
+    // Another way without putting [count] into useEffect watch list:
     const tick = () => {
-        setCount(count + 1);
+        setCount(prevState => prevState + 1);
     };
+
 
     // If you think dependent array as a way to specify when you want to run the useEffect, you will run into problems.
     // Instead, dependency array should be thought as a way to let React know about everything that useEffect must watch
@@ -13,13 +18,22 @@ const IntervalHookCounter = () => {
     // So but enter [], we tell React don't watch any variable change, just do it for the initial rendering.
     // Adding [count], it means we will want for count value change. And if it changes, we will  update the component
     // (the one int the return section)
+    // useEffect(() => {
+    //     const  interval = setInterval(tick, 1000);
+    //
+    //     return () => {
+    //         clearInterval(interval);
+    //     }
+    // }, [count]);
+
+    // Another way without putting [count] into useEffect watch list:
     useEffect(() => {
         const  interval = setInterval(tick, 1000);
 
         return () => {
             clearInterval(interval);
         }
-    }, [count]);
+    }, []);
 
     return (
         <div>
