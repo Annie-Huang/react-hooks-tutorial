e38542da904447aef453b0db277cdd2ea60c45c1
Annie Huang <annie.hn@gmail.com>
2019-12-29 17:40:36 +1100

    Create ClassCounterOne.js

--- effect-hook/src/App.js
+++ effect-hook/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ClassCounterOne from "./components/ClassCounterOne";
 
 function App() {
   return (
     <div className="App">
-
+        <ClassCounterOne />
     </div>
   );
 }
