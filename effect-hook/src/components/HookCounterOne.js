7d631665c160376fd5a5b2f05421169fa91ab5b9
Annie Huang <annie.hn@gmail.com>
2019-12-29 18:31:02 +1100

    Update similar code in HookCounterOne.js, you can still see useEffect is called when we typing in the input.

--- effect-hook/src/components/HookCounterOne.js
+++ effect-hook/src/components/HookCounterOne.js
@@ -2,15 +2,18 @@ import React, {useState, useEffect} from 'react';
 
 const HookCounterOne = () => {
     const [count, setCount]  = useState(0);
+    const [name, setName]  = useState('');
 
     // The passed in parameter is a function that is getting executed after render of every render of the component.
     // which means after the first render and after every update.
     useEffect(() => {
+        console.log('useEffect - Updating document title');
         document.title = `You clicked ${count} times`;
     });
 
     return (
         <div>
+            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
             <button onClick={() => setCount(count + 1)}>Click {count} times</button>
         </div>
     );
