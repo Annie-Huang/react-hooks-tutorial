190cefce79cdb38a60a9e42847c32ac7e91fe01c
Annie Huang <annie.hn@gmail.com>
2019-12-29 20:57:40 +1100

    Create a IntervalClassCounter.js component

--- effect-hook/src/App.js
+++ effect-hook/src/App.js
@@ -5,6 +5,7 @@ import HookCounterOne from "./components/HookCounterOne";
 import ClassMouse from "./components/ClassMouse";
 import HookMouse from "./components/HookMouse";
 import MouseContainer from "./components/MouseContainer";
+import IntervalClassCounter from "./components/IntervalClassCounter";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         {/*<HookCounterOne/>*/}
         {/*<ClassMouse />*/}
         {/*<HookMouse />*/}
-        <MouseContainer />
+        {/*<MouseContainer />*/}
+        <IntervalClassCounter />
     </div>
   );
 }
