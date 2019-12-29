491d6498cecaa2227f7bdf72468b69d363035b0e
Annie Huang <annie.hn@gmail.com>
2019-12-29 20:52:51 +1100

    Add compomentWillUnmount logic in both class component and Hooks.

--- effect-hook/src/components/ClassMouse.js
+++ effect-hook/src/components/ClassMouse.js
@@ -11,11 +11,15 @@ class ClassMouse extends Component {
     }
 
     logMousePosition = e => {
-        this.setState({ x: e.clientX, y: e.clientY })
+        this.setState({ x: e.clientX, y: e.clientY });
     };
 
     componentDidMount() {
-        window.addEventListener('mousemove', this.logMousePosition)
+        window.addEventListener('mousemove', this.logMousePosition);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('mousemove', this.logMousePosition);
     }
 
     render() {
--- effect-hook/src/components/HookMouse.js
+++ effect-hook/src/components/HookMouse.js
@@ -13,6 +13,16 @@ const HookMouse = () => {
     useEffect(() => {
         console.log('useEffect called');
         window.addEventListener('mousemove', logMousePosition);
+
+        // if not adding this code, you will get this error:
+        // index.js:1 Warning: Can't perform a React state update on an unmounted component.
+        // This is a no-op, but it indicates a memory leak in your application.
+        // To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
+        //     in HookMouse (at MouseContainer.js:9)
+        return () => {
+            console.log('Component unmouting code');
+            window.removeEventListener('mousemove', logMousePosition);
+        }
     }, []);
 
     return (
