5fcc7795f6abfdfef43ab0bb59b8ba5b57a91c84
Annie Huang <annie.hn@gmail.com>
2019-12-30 08:57:13 +1100

    Add UserContext.Provider in App.js and UserContext.Comsumer in ComponentF.js

--- context-hook/src/App.js
+++ context-hook/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import ComponentC from "./components/ComponentC";
 
+export const UserContext = React.createContext();
+
 function App() {
-  return (
-    <div className="App">
-        <ComponentC/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <UserContext.Provider value={'Vishwas'}>
+                <ComponentC/>
+            </UserContext.Provider>
+        </div>
+    );
 }
 
 export default App;
