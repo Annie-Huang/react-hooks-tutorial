28ecc28a23b07adc71b4e470162275806f17ef36
Annie Huang <annie.hn@gmail.com>
2019-12-29 21:30:25 +1100

    Adding extra notes.

--- effect-hook/src/components/IntervalHookCounter.js
+++ effect-hook/src/components/IntervalHookCounter.js
@@ -28,11 +28,31 @@ const IntervalHookCounter = () => {
 
     // Another way without putting [count] into useEffect watch list:
     useEffect(() => {
+        // It's recommended that when you need to call a function to do something inside useEffect,
+        // always defined the function inside useEffect, so you can easily adding the dependency (e.g. someProps)
+        // in the watchlist.
+        // https://twitter.com/dan_abramov/status/1098718196192997376?lang=en
+        // function doSomething() {
+        //     console.log(someProps);
+        // }
+        // doSomething();
+
+        // From Dan's comment:
+        // Doing
+        //
+        // useEffect(() => {
+        //   foo(props.something)
+        // }, [])
+        //
+        // and not specifying props.something in [] is *always* a bug.
+
         const  interval = setInterval(tick, 1000);
 
         return () => {
             clearInterval(interval);
         }
+
+        // You should have [someProps] below:
     }, []);
 
     return (
