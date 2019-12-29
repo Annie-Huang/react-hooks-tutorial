dc82bb3ddfa20b2d4ae9539bdf3055fbf10a3d64
Annie Huang <annie.hn@gmail.com>
2019-12-29 17:52:23 +1100

    Update HookCounterOne.js to use useEffect to execute some code after each render of the component.

--- effect-hook/src/components/HookCounterOne.js
+++ effect-hook/src/components/HookCounterOne.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const HookCounterOne = () => {
     const [count, setCount]  = useState(0);
+
+    // The passed in parameter is a function that is getting executed after render of every render of the component.
+    // which means after the first render and after every update.
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    });
 
     return (
         <div>
