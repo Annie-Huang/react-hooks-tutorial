22b09a2ef18b356769774b762847e4bda947a8db
Annie Huang <annie.hn@gmail.com>
2019-12-29 16:01:04 +1100

    Add explaination. And update Increment and Decrement function

--- state-hook/src/components/HookCounterTwo.js
+++ state-hook/src/components/HookCounterTwo.js
@@ -7,8 +7,12 @@ const HookCounterTwo = () => {
     const incrementFive = () => {
         for(let i=0; i<5; i++) {
             // setCount(count+1);
+
             // Any time you need to update the state value based on the previous state value,
             // always go for a safer option of passing a function that would set a new state value.
+
+            // https://reactjs.org/docs/hooks-reference.html#usestate
+            // If the new state is computed using the previous state, you can pass a function to setState. The function will receive the previous value, and return an updated value.
             setCount(prevState => prevState + 1);
         }
     };
@@ -17,8 +21,8 @@ const HookCounterTwo = () => {
         <div>
             Count: {count}
             <button onClick={() => setCount(initialCount)}>Reset</button>
-            <button onClick={() => setCount(count+1)}>Increment</button>
-            <button onClick={() => setCount(count-1)}>Decrement</button>
+            <button onClick={() => setCount(prevState => prevState + 1)}>Increment</button>
+            <button onClick={() => setCount(prevState => prevState - 1)}>Decrement</button>
             <button onClick={incrementFive}>Increment 5</button>
 
         </div>
