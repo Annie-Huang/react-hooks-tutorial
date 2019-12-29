2b82f2324e24a4c9ea39d2a53ea26a7e1cdc988c
Annie Huang <annie.hn@gmail.com>
2019-12-29 21:20:51 +1100

    Update notes

--- effect-hook/src/components/IntervalHookCounter.js
+++ effect-hook/src/components/IntervalHookCounter.js
@@ -11,6 +11,8 @@ const IntervalHookCounter = () => {
     // Instead, dependency array should be thought as a way to let React know about everything that useEffect must watch
     // for changes.
     // So but enter [], we tell React don't watch any variable change, just do it for the initial rendering.
+    // Adding [count], it means we will want for count value change. And if it changes, we will  update the component
+    // (the one int the return section)
     useEffect(() => {
         const  interval = setInterval(tick, 1000);
 
