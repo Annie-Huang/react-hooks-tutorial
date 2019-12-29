87b829d78bec6df2eceb0637c81db8bdfbf10f94
Annie Huang <annie.hn@gmail.com>
2019-12-30 09:43:05 +1100

    Create the reducer function and initialState in the useReducer method.

--- reducer-hook/src/components/CounterOne.js
+++ reducer-hook/src/components/CounterOne.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, {useReducer} from 'react';
+
+const initialState = 0;
+const reducer = (state, action) => {
+    // return newState;
+    switch (action) {
+        case 'increment':
+            return state + 1;
+        case 'decrement':
+            return state - 1;
+        case 'reset':
+            return initialState;
+        default:
+            return state;
+    }
+};
 
 const CounterOne = () => {
+    useReducer(reducer, initialState);
+
     return (
         <div>
             <button>Increment</button>
