4cba68501a2c684c9dccd3ead7b8902dd352c57a
Annie Huang <annie.hn@gmail.com>
2019-12-30 12:20:42 +1100

    Create the useReducer in app.js for counter (Copy from CounterOne.js)

--- reducer-hook/src/App.js
+++ reducer-hook/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import './App.css';
 import CounterOne from "./components/CounterOne";
 import CounterTwo from "./components/CounterTwo";
@@ -7,12 +7,29 @@ import ComponentA from "./components/ComponentA";
 import ComponentB from "./components/ComponentB";
 import ComponentC from "./components/ComponentC";
 
+const initialState = 0;
+const reducer = (state, action) => {
+    switch (action) {
+        case 'increment':
+            return state + 1;
+        case 'decrement':
+            return state - 1;
+        case 'reset':
+            return initialState;
+        default:
+            return state;
+    }
+};
+
 function App() {
+    const [count, dispatch] = useReducer(reducer, initialState);
+
     return (
         <div className="App">
             {/*<CounterOne />*/}
             {/*<CounterTwo />*/}
             {/*<CounterThree/>*/}
+            Count - {count}
             <ComponentA />
             <ComponentB />
             <ComponentC />
