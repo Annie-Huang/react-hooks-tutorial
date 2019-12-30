d5882323f292fd3b22a46d89875f9dc362b3e61c
Annie Huang <annie.hn@gmail.com>
2019-12-30 11:55:53 +1100

    Use object in state allow you to maintain multiple states.

--- reducer-hook/src/components/CounterTwo.js
+++ reducer-hook/src/components/CounterTwo.js
@@ -2,15 +2,20 @@ import React, {useReducer} from 'react';
 
 const initialState = {
     firstCounter: 0,
+    secondCounter: 10
 };
 const reducer = (state, action) => {
     switch (action.type) {
         case 'increment':
-            return {firstCounter: state.firstCounter + action.value};
+            return {...state, firstCounter: state.firstCounter + action.value};
         case 'decrement':
-            return  {firstCounter: state.firstCounter - action.value};
+            return  {...state, firstCounter: state.firstCounter - action.value};
+        case 'increment2':
+            return {...state, secondCounter: state.secondCounter + action.value};
+        case 'decrement2':
+            return  {...state, secondCounter: state.secondCounter - action.value};
         case 'reset':
-            return  {firstCounter: 0};
+            return initialState;
         default:
             return state;
     }
@@ -22,11 +27,16 @@ const CounterTwo = () => {
     return (
         <div>
             <div>Count - {count.firstCounter}</div>
+            <div>Count - {count.secondCounter}</div>
             <button onClick={() => dispatch({type: 'increment', value: 1})}>Increment</button>
             <button onClick={() => dispatch({type: 'decrement', value: 1})}>Decrement</button>
             <button onClick={() => dispatch({type: 'increment', value: 5})}>Increment 5</button>
             <button onClick={() => dispatch({type: 'decrement', value: 5})}>Decrement 5</button>
             <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
+            <div>
+                <button onClick={() => dispatch({type: 'increment2', value: 1})}>Increment Counter 2</button>
+                <button onClick={() => dispatch({type: 'decrement2', value: 1})}>Decrement Counter 2</button>
+            </div>
         </div>
     );
 };
