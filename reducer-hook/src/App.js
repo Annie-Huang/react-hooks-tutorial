d26304c9e768bd3a234423de0ea4e5ef492565d2
Annie Huang <annie.hn@gmail.com>
2019-12-30 12:17:19 +1100

    Create basic structure of ComponentA | ComponentB > ComponentD | ComponentC > ComponentE > ComponentF

--- reducer-hook/src/App.js
+++ reducer-hook/src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import CounterOne from "./components/CounterOne";
 import CounterTwo from "./components/CounterTwo";
 import CounterThree from "./components/CounterThree";
+import ComponentA from "./components/ComponentA";
+import ComponentB from "./components/ComponentB";
+import ComponentC from "./components/ComponentC";
 
 function App() {
     return (
         <div className="App">
             {/*<CounterOne />*/}
             {/*<CounterTwo />*/}
-            <CounterThree/>
+            {/*<CounterThree/>*/}
+            <ComponentA />
+            <ComponentB />
+            <ComponentC />
         </div>
     );
 }
