e2bac1ab19426c1dc08fbecc236faaf7015335fc
Annie Huang <annie.hn@gmail.com>
2019-12-30 15:41:31 +1100

    Explain why calling the incrementAge method will cause  increaseSalary method to be called as well

--- callback-hook/src/components/ParentComponent.js
+++ callback-hook/src/components/ParentComponent.js
@@ -7,6 +7,10 @@ function ParentComponent() {
     const [age, setAge] = useState(25);
     const [salary, setSalary] = useState(50000);
 
+    // a new incrementAge function is created when each time the parent component rerenders.
+    // when dealing with functions, we always need to consider of reference Equivity.
+    // So the function before the rerender is different from the function after the rerender because their pointer is different.
+    // And because the function is a props, the React see the props has changed, so rerender the child again.
     const incrementAge = () => {
         setAge(age + 1);
     };
