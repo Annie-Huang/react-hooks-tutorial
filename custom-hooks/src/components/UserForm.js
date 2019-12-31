16dfd52819edf58fe04e78a88d0fa2164db407cf
Annie Huang <annie.hn@gmail.com>
2019-12-31 18:11:26 +1100

    Change UserForm.js to use useInput instead. Very smart way to do binding.

--- custom-hooks/src/components/UserForm.js
+++ custom-hooks/src/components/UserForm.js
@@ -1,28 +1,41 @@
-import React, {useState} from 'react';
+import React from 'react';
+import useInput from "../hooks/useInput";
 
 const UserForm = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+    // const [firstName, setFirstName] = useState('');
+    // const [lastName, setLastName] = useState('');
+
+    const [firstName, bindFirstName, resetFirstName] = useInput('');
+    const [lastName, bindLastName, resetLastName] = useInput('');
 
     const submitHandler = e => {
         e.preventDefault();
         alert(`Hello ${firstName} ${lastName}`);
     };
 
+    // Very smart way to do binding. I never thought of doing it like this before.
     return (
         <div>
             <form onSubmit={submitHandler}>
                 <div>
                     <label>First name</label>
+                    {/*<input*/}
+                    {/*    value={firstName}*/}
+                    {/*    onChange={e => setFirstName(e.target.value)}*/}
+                    {/*    type="text"*/}
+                    {/*/>*/}
                     <input
-                        value={firstName}
-                        onChange={e => setFirstName(e.target.value)}
+                        {...bindFirstName}
                         type="text"
                     />
                     <label>Last name</label>
+                    {/*<input*/}
+                    {/*    value={lastName}*/}
+                    {/*    onChange={e => setLastName(e.target.value)}*/}
+                    {/*    type="text"*/}
+                    {/*/>*/}
                     <input
-                        value={lastName}
-                        onChange={e => setLastName(e.target.value)}
+                        {...bindLastName}
                         type="text"
                     />
                 </div>
