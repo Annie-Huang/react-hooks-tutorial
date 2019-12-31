f4017ab999eca90712bb54f55813bcdb6e8d611f
Annie Huang <annie.hn@gmail.com>
2019-12-31 18:04:01 +1100

    Create the bind function and return array set.

--- custom-hooks/src/hooks/useInput.js
+++ custom-hooks/src/hooks/useInput.js
@@ -5,6 +5,13 @@ const useInput = (initialValue) => {
     const reset = () => {
         setValue(initialValue);
     };
+
+    const bind = {
+        value,
+        onChange: e => setValue(e.target.value)
+    };
+
+    return [value, bind, reset];
 };
 
 export default useInput;
