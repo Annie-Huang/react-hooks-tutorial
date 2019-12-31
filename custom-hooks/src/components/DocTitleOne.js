f5fd69423f0349d75c616526a7cf5e29ae27d429
Annie Huang <annie.hn@gmail.com>
2019-12-31 17:17:19 +1100

    Call useDocumentTitle from DocTitleOne.js and DocTitleTwo.js. But I can see count value does not persistent between the two components.

--- custom-hooks/src/components/DocTitleOne.js
+++ custom-hooks/src/components/DocTitleOne.js
@@ -1,12 +1,15 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
+import useDocumentTitle from "../hooks/useDocumentTitle";
 
 const DocTitleOne = () => {
     const [count, setCount] = useState(0);
 
-    // Updating the document title is a side effect so the code goes within the useEffect hook.
-    useEffect(() => {
-        document.title = `Count ${count}`;
-    }, [count]);
+    // // Updating the document title is a side effect so the code goes within the useEffect hook.
+    // useEffect(() => {
+    //     document.title = `Count ${count}`;
+    // }, [count]);
+    //
+    useDocumentTitle(count);
 
     return (
         <div>
