dc35a687686289edfe4f27e87f0ac5d00cda37da
Annie Huang <annie.hn@gmail.com>
2019-12-31 16:37:18 +1100

    Cannot use interval because it will cause error when reference outside useEffect. Use useRef instead to solve the problem

--- ref-hook/src/components/HookTimer.js
+++ ref-hook/src/components/HookTimer.js
@@ -1,22 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 const HookTimer = () => {
     const [timer, setTimer] = useState(0);
+    const intervalRef = useRef();
 
     useEffect(() => {
-        const interval = setInterval(() => {
+        intervalRef.current = setInterval(() => {
             setTimer(prevTimer => prevTimer + 1);
         }, 1000);
 
         return () => {
-            clearInterval(interval);
+            clearInterval(intervalRef.current);
         }
 
     }, [])
 
+    // useRef can hold to a reference to the DOM node by using the ref attribute, it can also used to store any mutable value.
+    // The value will persist through rerender while also not causing any additional renders when its value changes
     return (
         <div>
             Hook Timer - {timer}
+            <button onClick={() => clearInterval(intervalRef.current)}>Clear Hook Timer</button>
         </div>
     );
 };
