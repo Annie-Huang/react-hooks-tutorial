4f3e1675e23ad2f5210c59406e278a6a43ebfe6c
Annie Huang <annie.hn@gmail.com>
2019-12-31 17:13:56 +1100

    Need to keep the stateless function name small letter. Very strange convention for a file.

--- custom-hooks/src/hooks/useDocumentTitle.js
+++ custom-hooks/src/hooks/useDocumentTitle.js
@@ -1,10 +1,10 @@
 import {useEffect} from 'react';
 
-const UseDocumentTitle = (count) => {
+const useDocumentTitle = (count) => {
     // Updating the document title is a side effect so the code goes within the useEffect hook.
     useEffect(() => {
         document.title = `Count ${count}`;
     }, [count]);
 };
 
-export default UseDocumentTitle;
+export default useDocumentTitle;
